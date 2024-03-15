f641e54307822ec79f453eb7b22dd52396a9e710
Swotch <109398760+Swotch9399@users.noreply.github.com>
2024-03-15 18:01:40 +0300

    Update accounts.js

--- config/accounts.js
+++ config/accounts.js
@@ -2,8 +2,8 @@ var configsArray = [];
 var config;
 
 config = {};
-config.username = 'taylan20081';
-config.password = 'Taylan2008';
+config.username = process.env.USERNAME1;
+config.password = process.env.PASSWORD1;
 config.sharedSecret = '';
 config.enableStatus = true;
 config.gamesAndStatus = [218,271590,1174180,447040,1274570,739630,1158940,218620,394360,1551360,1293830,252490,359550,255710,1811260,227300,1938090,236390,1250410,239140,4000,534380,1326470,815370,244210,242760,270880,814380,730,1144200,311210,107410];
